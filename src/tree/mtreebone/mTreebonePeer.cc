4a21619d7333da118930061c81ed78b0d354aa42
senft <senftt@gmail.com>
2013-04-28 13:41:01 +0200

    Revert "Added a skeleton for mTreebone"
    This reverts commit d7ebb49e45ed8fd798b44337a9a12fa09ca9b13d.

--- src/tree/mtreebone/mTreebonePeer.cc
+++
@@ -1,9 +0,0 @@
-#include "mTreebonePeer.h"
-
-Define_Module(mTreebonePeer);
-
-mTreebonePeer::mTreebonePeer(){
-}
-
-mTreebonePeer::~mTreebonePeer(){
-}
--- src/tree/mtreebone/mTreeboneSource.cc
+++
@@ -1,9 +0,0 @@
-#include "mTreeboneSource.h"
-
-Define_Module(mTreeboneSource);
-
-mTreeboneSource::mTreeboneSource(){
-}
-
-mTreeboneSource::~mTreeboneSource(){
-}
