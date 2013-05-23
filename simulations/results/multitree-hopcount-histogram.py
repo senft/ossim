6e3810375c3612face31ebe32947b6633479d372
senft <senftt@gmail.com>
2013-05-23 18:06:17 +0200

    Added small script to create a histogram for the hopcount

---
+++ simulations/results/multitree-hopcount-histogram.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from subprocess import call
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    call('scavetool vector -p "*hop*" -O hopcount.csv -F csv Multitree_Network-0.vec',
+            shell=True)
+
+    with open('hopcount.csv') as f:
+        v = np.loadtxt(f, delimiter=",", dtype='int', skiprows=1, usecols=(1,))
+
+    # Get rid of hopcounts < 1 (creation of packets is recorded in the source,
+    # too)
+    v = [n for n in v if n > 0]
+
+    bins = sorted(set(n for n in v))
+
+    fig, ax = plt.subplots(1, 1)
+
+    ax.xaxis.set_ticks(bins[:-1])
+
+    ax.xaxis.set_label_text('Hopcount')
+    ax.yaxis.set_label_text('Probability')
+
+    ax.grid(True)
+
+    ax.hist(v, normed=True, bins=bins, facecolor='green', alpha=0.75,
+            align='left', rwidth=6.0/len(bins))
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
