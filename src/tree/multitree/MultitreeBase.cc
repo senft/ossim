6ecb5a0aba718e68e30b69f227e69eec322a31b3
senft <senftt@gmail.com>
2013-07-07 22:33:49 +0200

    Fixed cost function

--- src/tree/multitree/MultitreeBase.cc
+++ src/tree/multitree/MultitreeBase.cc
@@ -558,12 +558,13 @@ double MultitreeBase::getDepencyCosts(IPvXAddress child) // K_4
 
 	for (int i = 0; i < numStripes; i++)
 	{
-		std::set<IPvXAddress> curDisconnectingChildren = disconnectingChildren[i];
-       	std::vector<IPvXAddress> curChildren = m_partnerList->getChildren(i);
+		std::set<IPvXAddress> &curDisconnectingChildren = disconnectingChildren[i];
+       	std::vector<IPvXAddress> &curChildren = m_partnerList->getChildren(i);
+
        	for(std::vector<IPvXAddress>::iterator it = curChildren.begin(); it != curChildren.end(); ++it)
        	{
-			IPvXAddress child = (IPvXAddress)*it;
-			if ( child.equals(child) && curDisconnectingChildren.find(child) == curDisconnectingChildren.end() )
+			IPvXAddress curChild = (IPvXAddress)*it;
+			if ( child.equals(curChild) && curDisconnectingChildren.find(child) == curDisconnectingChildren.end() )
 			{
 				numConnections++;
 				break;
